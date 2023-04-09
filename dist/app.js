f596871d5701ba1cf363c8a0462e218cebc58c78
whiskacz <pawelblasiak1985@gmail.com>
2023-04-09 18:34:12 +0200

    TS practice Array

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,48 @@
-const button = document.querySelector('button');
-const calculatePrice = (orginalPrice, discount) => {
-    return discount ? orginalPrice * 0.8 : orginalPrice;
+const inputElement = document.querySelector("#name");
+const buttonElement = document.querySelector("button");
+const taskContainer = document.querySelector(".tasks");
+const tasks = [
+    {
+        name: "make a homework",
+        done: false
+    },
+    {
+        name: "buy a car",
+        done: true
+    },
+    {
+        name: "build a house",
+        done: false
+    }
+];
+const render = () => {
+    taskContainer.innerHTML = "";
+    inputElement.value = "";
+    tasks.forEach((task, index) => {
+        const id = `task-${index}`;
+        const taskElement = document.createElement("li");
+        const labelElement = document.createElement("label");
+        const checkElement = document.createElement("input");
+        labelElement.innerText = task.name;
+        labelElement.setAttribute("for", id);
+        taskContainer.appendChild(taskElement);
+        taskElement.appendChild(labelElement);
+        taskElement.appendChild(checkElement);
+        checkElement.type = "checkbox";
+        checkElement.id = id;
+        checkElement.checked = task.done;
+        checkElement.addEventListener("change", () => {
+            task.done = !task.done;
+        });
+    });
 };
-button.addEventListener("click", () => {
-    const orginalPrice = 50;
-    const discount = new URLSearchParams(window.location.search).get("discount") === "true";
-    console.log(discount);
-    console.log(calculatePrice(orginalPrice, discount));
+render();
+const AddTask = (taskName) => {
+    tasks.push({ name: taskName, done: false });
+};
+buttonElement.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(inputElement.value);
+    AddTask(inputElement.value);
+    render();
 });
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,58 @@
-const button = document.querySelector('button');
+const inputElement: HTMLInputElement = document.querySelector("#name")
+const buttonElement: HTMLButtonElement = document.querySelector("button")
+const taskContainer: HTMLElement = document.querySelector(".tasks")
 
-const calculatePrice = (orginalPrice: number, discount: boolean) => {
-   return discount ? orginalPrice * 0.8 : orginalPrice;
+const tasks: { 
+    name: string;
+    done: boolean;
+}[] = [
+    {
+    name: "make a homework",
+    done: false
+},
+{   
+    name: "buy a car",
+    done: true
+},
+{
+    name: "build a house",
+    done: false
+}
+]
+const render = () => { 
+    taskContainer.innerHTML = ""  
+    inputElement.value = ""
+tasks.forEach((task, index) => {
+    const id:string = `task-${index}`
+    const taskElement: HTMLElement = document.createElement("li")
+    const labelElement: HTMLLabelElement = document.createElement("label")
+    const checkElement: HTMLInputElement = document.createElement("input")
+    
+    labelElement.innerText = task.name
+    labelElement.setAttribute("for", id)
+
+    taskContainer.appendChild(taskElement)
+    taskElement.appendChild(labelElement)
+    taskElement.appendChild(checkElement)
+
+    checkElement.type = "checkbox"
+    checkElement.id = id
+    checkElement.checked = task.done
+    checkElement.addEventListener("change", () =>{
+            task.done = !task.done
+    })
+
+})
+}
+render()
+
+const AddTask = (taskName: string) => {
+    tasks.push({name: taskName, done: false})
 }
 
-button.addEventListener("click", () => {
-   const orginalPrice: number = 50;
-   const discount: boolean = new URLSearchParams(window.location.search).get("discount") === "true";
-   console.log(discount)
-   console.log(calculatePrice(orginalPrice, discount))
-});
+buttonElement.addEventListener("click", (e) =>{
+    e.preventDefault()
+    console.log(inputElement.value)
+    AddTask(inputElement.value)
+    render()
+})
